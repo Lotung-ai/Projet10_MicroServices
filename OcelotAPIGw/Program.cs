ce636f18fa7f0c4219e8d0f5c98cfd9bce4560bd
Lotung <Lotung@gmail.com>
2024-08-13 12:07:23 +0200

    Ajout de la view dans le MicroFrontEnd Ajout de la configuration ocelot

--- OcelotAPIGw/Program.cs
+++ OcelotAPIGw/Program.cs
@@ -1,8 +1,13 @@
+using Ocelot.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Add ocelot configuration file and injecting ocelot service
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
